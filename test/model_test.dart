8a2181c8c8007a9d535ad2d352fcadad55038bd3
Martin Kamleithner <martin+kamleithner@diagnosia.com>
2020-10-10 16:44:37 +0200

    more tests

---
+++ test/model_test.dart
@@ -0,0 +1,39 @@
+import 'package:diffutil_dart/diffutil.dart' as diffutil;
+import 'package:test/test.dart';
+
+void main() {
+  test('when() works', () {
+    const updates = <diffutil.DiffUpdate>[
+      diffutil.Insert(count: 1, position: 1),
+      diffutil.Remove(count: 1, position: 1),
+      diffutil.Change(position: 1),
+      diffutil.Move(from: 1, to: 2),
+    ];
+
+    for (final update in updates) {
+      update.when(
+        insert: (count, pos) => expect(update, isA<diffutil.Insert>()),
+        remove: (count, pos) => expect(update, isA<diffutil.Remove>()),
+        change: (count, pos) => expect(update, isA<diffutil.Change>()),
+        move: (from, to) => expect(update, isA<diffutil.Move>()),
+      );
+    }
+  });
+
+  test('equals/hashcode works', () {
+    expect({
+      diffutil.Insert(count: 1, position: 1),
+      diffutil.Insert(count: 1, position: 1),
+    }, hasLength(1));
+    expect({
+      diffutil.Remove(count: 1, position: 1),
+      diffutil.Remove(count: 1, position: 1),
+    }, hasLength(1));
+    expect({
+      diffutil.Change(position: 1), diffutil.Change(position: 1),
+    }, hasLength(1));
+    expect({
+      diffutil.Move(from: 1, to: 2), diffutil.Move(from: 1, to: 2),
+    }, hasLength(1));
+  });
+}
