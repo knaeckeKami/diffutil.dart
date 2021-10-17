c8d856c5706a16edc485a1c040435b75c2f9c5a3
Martin Kamleithner <martin+kamleithner@diagnosia.com>
2021-10-17 22:15:20 +0200

    dart format

--- test/data_model_test.dart
+++ test/data_model_test.dart
@@ -5,7 +5,7 @@ void main() {
   test('when() works', () {
     const updates = <diffutil.DataDiffUpdate<int>>[
       diffutil.DataInsert(position: 0, data: 0),
-      diffutil.DataRemove( position: 1, data: 1),
+      diffutil.DataRemove(position: 1, data: 1),
       diffutil.DataChange(position: 1, newData: 2, oldData: 3),
       diffutil.DataMove(from: 1, to: 2, data: 4),
     ];
@@ -30,21 +30,21 @@ void main() {
         },
         remove: (pos, data) => fail("should not be remove"),
         change: (pos, oldData, newData) => fail("should not be change"),
-        move: (from, to, data) => fail("should not be move"),);
+        move: (from, to, data) => fail("should not be move"),
+      );
     });
 
     test('when() remove', () {
       const diffutil.DataRemove(position: 0, data: 1).when(
         insert: (pos, data) => fail("should not be insert"),
-        remove: (pos, data) =>
-            () {
+        remove: (pos, data) => () {
           expect(pos, 0);
           expect(data, 1);
         },
         change: (pos, oldData, newData) => fail("should not be change"),
-        move: (from, to, data) => fail("should not be move"),);
+        move: (from, to, data) => fail("should not be move"),
+      );
     });
-
 
     test('when() change', () {
       const diffutil.DataChange(position: 0, oldData: 1, newData: 2).when(
@@ -55,14 +55,13 @@ void main() {
           expect(oldData, 1);
           expect(newData, 2);
         },
-        move: (from, to, data) => fail("should not be move"),);
+        move: (from, to, data) => fail("should not be move"),
+      );
     });
-
 
     test('when() move', () {
       const diffutil.DataMove(from: 1, to: 2, data: 3).when(
           insert: (pos, data) => fail("should not be insert"),
-
           remove: (pos, data) => fail("should not be remove"),
           change: (pos, oldData, newData) => fail("should not be change"),
           move: (from, to, data) {
@@ -73,23 +72,24 @@ void main() {
     });
   });
 
-
   test('equals/hashcode works', () {
     expect({
       const diffutil.Insert(count: 1, position: 1),
-      const diffutil.Insert(count: 1, position: 1),   //ignore: equal_elements_in_set
+      const diffutil.Insert(
+          count: 1, position: 1), //ignore: equal_elements_in_set
     }, hasLength(1));
     expect({
       const diffutil.Remove(count: 1, position: 1),
-      const diffutil.Remove(count: 1, position: 1),   //ignore: equal_elements_in_set
+      const diffutil.Remove(
+          count: 1, position: 1), //ignore: equal_elements_in_set
     }, hasLength(1));
     expect({
       const diffutil.Change(position: 1),
-      const diffutil.Change(position: 1),   //ignore: equal_elements_in_set
+      const diffutil.Change(position: 1), //ignore: equal_elements_in_set
     }, hasLength(1));
     expect({
       const diffutil.Move(from: 1, to: 2),
-      const diffutil.Move(from: 1, to: 2),   //ignore: equal_elements_in_set
+      const diffutil.Move(from: 1, to: 2), //ignore: equal_elements_in_set
     }, hasLength(1));
   });
 }
